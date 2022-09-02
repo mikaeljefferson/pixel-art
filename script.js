5bed7ea62f01d1516da25d44618b2860ad1e8f01
mikael jefferson da silva <mikaeljeffeson@gmail.com>
2022-09-02 16:03:49 -0300

    quesito6test

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ window.onload =function() {
   function button() {
       const clickPaleta = document.querySelectorAll('.color');
       const newButton = document.getElementById('button-random-color');
+      
       newButton.addEventListener('click', () => {
         for (let index = 0; index < clickPaleta.length; index += 1) {
           const cor1 = Math.floor(Math.random() * 255);
           const cor2 = Math.floor(Math.random() * 255);
           const cor3 = Math.floor(Math.random() * 255);
-    
+          localStorage.setItem('colorPalette', JSON.stringify(newButton));
           if (index === 0) {
             clickPaleta[index].style.backgroundColor = 'black';
           } else {
             clickPaleta[index].style.backgroundColor = `rgb(${cor1}, ${cor2}, ${cor3})`;
           }
+          let savecolorPalette = localStorage.getItem('colorPalette');
         }
       });
     }
     button();
+    
     
   }
